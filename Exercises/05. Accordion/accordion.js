2cdd136cc7b46f54f3d61de245af12d7a55a002b
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-07-09 21:19:31 +0300

    add

--- Exercises/05. Accordion/accordion.js
+++ Exercises/05. Accordion/accordion.js
@@ -5,21 +5,81 @@
         const response = await fetch(url);
         const dataTitle = await response.json();
 
-        console.log(dataTitle[0]);
+        Object.values(dataTitle).map(revalContent).forEach(o => spanElement.textContent(o));
 
     } catch (e) {
         console.error(e.name + ': ' + e.message);
     }
+
 })();
 
-(function revalContent(){
+function revalContent(data) {
 
-    let divHeadElement = document.createElement('div').classList.add('head');
-    // divHeadElement.addClass
-    
+    let divAccElement = document.createElement('div');
+    divAccElement.classList.add('accordion');
+
+    let divHeadElement = document.createElement('div');
+    divHeadElement.classList.add('head');
+
+    let spanElement = document.createElement('span');
+    spanElement.textContent = data.title;
+
+    let buttonElement = document.createElement('button');
+    buttonElement.classList.add('button');
+    buttonElement.setAttribute('id', data._id);
+    buttonElement.textContent = ('More');
+    buttonElement.addEventListener('click', buttonTest);
+
+    divHeadElement.appendChild(spanElement);
+    divHeadElement.appendChild(buttonElement);
+
+    let divExtraElement = document.createElement('div');
+    divExtraElement.classList.add('extra');
+
+    divAccElement.appendChild(divHeadElement);
+    divAccElement.appendChild(divExtraElement);
+
     let mainSection = document.querySelector('#main');
-    mainSection.appendChild(divHeadElement);
+    mainSection.appendChild(divAccElement);
 
     return mainSection;
+}
 
-})();
+function buttonTest(e) {
+    let addContent = e.target.parentElement.nextElementSibling;
+    addContent.innerHTML = '';
+    addContent = e.target.parentElement.nextElementSibling;
+
+    let buttonId = e.target.id;
+
+    let showMoreButton = e.target;
+    showMoreButton.textContent = showMoreButton.textContent === 'More'
+        ? 'Less'
+        : 'More';
+
+    let hideExtraClass = e.target.parentElement.nextElementSibling;
+
+    if (showMoreButton.textContent == 'More') {
+        hideExtraClass.setAttribute("style", "display: none;");
+    } else {
+        hideExtraClass.setAttribute("style", "display: block;");
+    }
+
+    (async function solution2() {
+        const info = ('http://localhost:3030/jsonstore/advanced/articles/details/' + buttonId);
+
+        try {
+            const responseInfo = await fetch(info);
+            const dataInfo = await responseInfo.json();
+
+            let pElement = document.createElement('p');
+
+            pElement.textContent = dataInfo.content;
+
+            addContent.appendChild(pElement);
+
+        } catch (e) {
+            console.error(e.name + ': ' + e.message);
+        }
+    })();
+}
