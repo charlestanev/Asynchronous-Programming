25d7b534f04ab76c0656a0bf3dd16443b9532ddc
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-07-09 15:40:18 +0300

    add

--- Exercises/05. Accordion/accordion.js
+++ Exercises/05. Accordion/accordion.js
@@ -1,3 +1,25 @@
-function solution() {
-    //TODO .....
-}
+(async function solution() {
+    const url = ('http://localhost:3030/jsonstore/advanced/articles/list');
+
+    try {
+        const response = await fetch(url);
+        const dataTitle = await response.json();
+
+        console.log(dataTitle[0]);
+
+    } catch (e) {
+        console.error(e.name + ': ' + e.message);
+    }
+})();
+
+(function revalContent(){
+
+    let divHeadElement = document.createElement('div').classList.add('head');
+    // divHeadElement.addClass
+    
+    let mainSection = document.querySelector('#main');
+    mainSection.appendChild(divHeadElement);
+
+    return mainSection;
+
+})();
