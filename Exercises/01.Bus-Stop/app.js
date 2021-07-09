770ff87dc1fad998fab7b135538783f136264156
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-07-09 10:35:17 +0300

    add

--- Exercises/01.Bus-Stop/app.js
+++ Exercises/01.Bus-Stop/app.js
@@ -1,28 +1,61 @@
-function getInfo() {
-    let stopIdInput = document.getElementById('stopId');
-    let stopId = stopIdInput.value;
+// // First Way
+async function getInfo() {
+    const input = document.getElementById('stopId');
+    const id = input.value;
 
-    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
-    .then(body => body.json())
-    .then(stopInfo => {
-        let stopNameDiv = document.getElementById('stopName');
-        stopNameDiv.textContent = stopInfo.name;
-        
-        let busesUl = document.getElementById('buses');
+    const url = 'http://localhost:3030/jsonstore/bus/businfo/' + id;
 
-        Array.from(busesUl.querySelectorAll('li')).forEach(li => li.remove());
+    try {
+        const ulElement = document.getElementById('buses');
+        ulElement.innerHTML = '';
 
-        Object.keys(stopInfo.buses).forEach( key => {
-            let busInfoLi = document.createElement('li');
-            busInfoLi.textContent = `Bus ${key} arrives in ${stopInfo.buses[key]}`;
+        const response = await fetch(url);
+        const data = await response.json();
 
-            busesUl.appendChild(busInfoLi);
-        })
-    })
-    .catch(err => {
-        let stopNameDiv = document.getElementById('stopName');
-        stopNameDiv.textContent = `Error`;
-        let busesUl = document.getElementById('buses');
-        Array.from(busesUl.querySelectorAll('li')).forEach(li => li.remove());
-    })
+        document.getElementById('stopName').textContent = data.name;
+
+        Object.entries(data.buses).map((([bus, time]) => {
+            const result = document.createElement('li');
+            result.textContent = `Bus ${bus} arrives in ${time} minutes`;
+
+            ulElement.appendChild(result);
+        }))
+
+        input.value = '';
+    } catch (error) {
+        document.getElementById('stopName').textContent = 'Error';
+    }
 }
+
+
+
+
+// // Second Way
+// function getInfo() {
+//     let stopIdInput = document.getElementById('stopId');
+//     let stopId = stopIdInput.value;
+
+//     fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+//     .then(body => body.json())
+//     .then(stopInfo => {
+//         let stopNameDiv = document.getElementById('stopName');
+//         stopNameDiv.textContent = stopInfo.name;
+
+//         let busesUl = document.getElementById('buses');
+
+//         Array.from(busesUl.querySelectorAll('li')).forEach(li => li.remove());
+
+//         Object.keys(stopInfo.buses).forEach( key => {
+//             let busInfoLi = document.createElement('li');
+//             busInfoLi.textContent = `Bus ${key} arrives in ${stopInfo.buses[key]}`;
+
+//             busesUl.appendChild(busInfoLi);
+//         })
+//     })
+//     .catch(err => {
+//         let stopNameDiv = document.getElementById('stopName');
+//         stopNameDiv.textContent = `Error`;
+//         let busesUl = document.getElementById('buses');
+//         Array.from(busesUl.querySelectorAll('li')).forEach(li => li.remove());
+//     })
+// }
